a28b2832b0da382fd9415157413a78a7559f30b7
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-05-09 21:12:51 +0200

    switch logo

--- content/conf.py
+++ content/conf.py
@@ -67,7 +67,7 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_logo = "img/ENCCS.jpg"
+html_logo = "img/ENCCS-NordIQuEst.jpg"
 html_favicon = "img/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
